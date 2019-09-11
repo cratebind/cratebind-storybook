5bd25ac17a52690b401051c4edeb6d5546cac100
Matt Wood <matt@cratebind.com>
2019-09-11 09:05:22 -0500

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+// const error = 2;
+// const warn = 1;
+const ignore = 0;
+
+module.exports = {
+  extends: ['cratebind'],
+  rules: {
+    'prettier/prettier': [
+      'warn',
+      {
+        printWidth: 100,
+        tabWidth: 2,
+        bracketSpacing: true,
+        trailingComma: 'es5',
+        singleQuote: true,
+        jsxBracketSameLine: false,
+      },
+    ],
+    'import/prefer-default-export': ignore,
+    'react/jsx-filename-extension': [
+      1,
+      {
+        extensions: ['.js'],
+      },
+    ],
+    'react/jsx-wrap-multilines': [
+      'error',
+      {
+        arrow: false,
+      },
+    ],
+    'import/no-extraneous-dependencies': ['error', { devDependencies: ['**/*.stories.js'] }],
+  },
+};
